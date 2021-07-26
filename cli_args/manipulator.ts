f33401e281724798c12675efea0356b89e7cf90a
Jan Merhar <merharjan@gmail.com>
2021-07-26 18:23:09 +0200

    Added cli_args for new classes

---
+++ cli_args/manipulator.ts
@@ -0,0 +1,40 @@
+import { Command } from "commander"
+import { CommandFunctions } from "./cli_class"
+
+const manipulatorManCommander = new Command()
+
+/*
+ * REQUIRED OPTIONS
+ */
+manipulatorManCommander
+  .requiredOption("-i, --input <string>", "Filename that will be imported")
+  .requiredOption(
+    "-tof, --type-of-file <number>",
+    "1 => txt file, 2 => csv file, 3 => txt and csv file",
+    "3"
+  )
+
+/*
+ * OPTIONAL OPTIONS
+ */
+// I may implement optional output path
+manipulatorManCommander
+  .option(
+    "-at, --anki-tag <string>",
+    "Input tag field value for imported document: eg. OMA-13"
+  )
+  .option(
+    "-f, --file-name <string>",
+    "Name of the output file",
+    CommandFunctions.fileWithoutExtension
+  )
+  .option(
+    "-r, --run <number>",
+    "1 => runs manipulated data in program,  0 => doesn't run",
+    "1"
+  )
+
+manipulatorManCommander.parse()
+// console.log(regexManCommander.opts())
+
+export { manipulatorManCommander }
---
+++ execs/manipulator.ts
@@ -0,0 +1,10 @@
+import { manipulatorManCommander } from "../cli_args/manipulator"
+import { LaTeXManipulator } from "../manipulator/LaTeXManipulator"
+import { MDManipulator } from "../manipulator/MDManipulator"
+
+/*
+regexManCommander.opts().input
+regexManCommander.opts().fileName
+regexManCommander.opts().ankiTag
+regexManCommander.opts().run
+*/
