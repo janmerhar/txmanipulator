fbf5b07a632399e7cfc26fd6e282586b819d1650
Jan Merhar <merharjan@gmail.com>
2021-09-11 15:47:07 +0200

    Cleaned template.tx code

--- execs/template.ts
+++ execs/template.ts
@@ -1,15 +1,12 @@
 import { LaTeXTemplate } from "../template/LaTeXTemplate"
 import { templateCommander } from "../cli_args/cli_template"
 
-const latex = new LaTeXTemplate(templateCommander.opts().fileName)
+const templateOpts = templateCommander.opts()
+const latex = new LaTeXTemplate(templateOpts.fileName)
 
-latex.setPackages(templateCommander.opts().packages)
-latex.setTitleData(
-  templateCommander.opts().author,
-  templateCommander.opts().date,
-  templateCommander.opts().title
-)
+latex.setPackages(templateOpts.packages)
+latex.setTitleData(templateOpts.author, templateOpts.date, templateOpts.title)
 latex.setBeginDocument()
-latex.setSections(templateCommander.opts().sections)
+latex.setSections(templateOpts.sections)
 latex.writeToFile()
 latex.openCreatedDocument()
